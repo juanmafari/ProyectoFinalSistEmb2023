f03eb5238cefa25096d497ac18b9e7f77fb73126
juanmafari <juan.m.ottonelli@gmail.com>
2023-06-29 20:11:27 -0300

    no puede recibir hostname por softap

--- proyefinal2023/main/main.c
+++ proyefinal2023/main/main.c
@@ -8,6 +8,15 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_netif.h"
+#include "esp_netif_types.h"
+#include "lwip/ip_addr.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+//#include "mdns.h"
+#include "mqtt_client.h"
 
 
 
@@ -16,8 +25,8 @@
    If you'd rather not, just change the below entries to strings with the config you want
    - ie #define EXAMPLE_ESP_WIFI_SSID "mywifissid" */
 
-#define EXAMPLE_ESP_WIFI_SSID_STA      "caliope"
-#define EXAMPLE_ESP_WIFI_PASS_STA      "sinlugar"
+#define EXAMPLE_ESP_WIFI_SSID_STA      "MaryLuWiFi-2.4GHz"
+#define EXAMPLE_ESP_WIFI_PASS_STA      "lu5291278"
 #define EXAMPLE_ESP_MAXIMUM_RETRY_STA  20
 
 #define EXAMPLE_ESP_WIFI_SSID_AP      "kaluga_wifi"
@@ -34,6 +43,8 @@ static const char *TAG_STA = "wifi station";
 static const char *TAG_AP = "wifi softAP";
 
 static int s_retry_num_sta = 0;
+
+static esp_netif_t *s_ap_netif ;
 
 
 
@@ -139,7 +150,7 @@ void wifi_init_softap(void)
     if (strlen(EXAMPLE_ESP_WIFI_PASS_AP) == 0) {
         wifi_config_ap.ap.authmode = WIFI_AUTH_OPEN;
     }
-
+    
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config_ap));
 
@@ -149,6 +160,117 @@ void wifi_init_softap(void)
 
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
 }
+
+
+
+#define BROKER_URL "mqtt://demo.thingsboard.io"
+#define BROKER_PORT 1883
+#define CLIENT_ID "clientId-1I2csjHPrr"
+#define TOPIC "v1/devices/me/telemetry"
+#define BROKER_USER "79p8v7vt7232m1fzff2t"
+#define BROKER_PASS ""
+
+
+
+static const char *TAG = "MQTT_EXAMPLE";
+
+esp_mqtt_client_handle_t client;
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+
+
+void publish_telemetry(esp_mqtt_client_handle_t client, const char* topic, const char* data)
+{
+    esp_mqtt_client_publish(client, topic, data, strlen(data), 0, 0);
+}
+
+
+
+void mqtt_app_start(void)
+{
+    esp_err_t err = esp_wifi_stop();
+
+    
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URL,
+        .broker.address.port = BROKER_PORT,
+        .credentials.username = BROKER_USER,
+        .credentials.authentication.password = BROKER_PASS,
+        
+
+        //.credentials.client_id = CLIENT_ID,
+        //.event_handler = mqtt_event_handler_cb,
+
+    };
+    client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+    
+}
+
 
 
 void app_main(void)
@@ -165,4 +287,19 @@ void app_main(void)
 
     ESP_LOGI(TAG_AP, "ESP_WIFI_MODE_AP");
     wifi_init_softap();
+
+    
+    
+   
+
+    
+    
+    mqtt_app_start();
+    
+
+    while (1){
+
+        publish_telemetry(client, TOPIC, "{\"temperature\": 24.5, \"humidity\": 70.2}");
+        vTaskDelay(5000/portTICK_PERIOD_MS);
+    }
 }
