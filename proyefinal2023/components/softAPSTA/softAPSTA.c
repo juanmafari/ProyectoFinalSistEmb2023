b552c2e6ec05f2e8f42f9ee3f7e9bd06ac221530
juanmafari <juan.m.ottonelli@gmail.com>
2023-06-30 00:29:30 -0300

    emprolijado de main, APSTA y MQTT en components

---
+++ proyefinal2023/components/softAPSTA/softAPSTA.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include "softAPSTA.h"
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_netif_types.h"
+#include "lwip/ip_addr.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+/* The examples use WiFi configuration that you can set via project configuration menu
+   If you'd rather not, just change the below entries to strings with the config you want
+   - ie #define EXAMPLE_ESP_WIFI_SSID "mywifissid" */
+
+#define EXAMPLE_ESP_WIFI_SSID_STA      "MaryLuWiFi-2.4GHz"
+#define EXAMPLE_ESP_WIFI_PASS_STA      "lu5291278"
+
+
+#define EXAMPLE_ESP_WIFI_SSID_AP      "kaluga_wifi"
+#define EXAMPLE_ESP_WIFI_PASS_AP      "kalugaucu1"
+
+#define EXAMPLE_ESP_MAXIMUM_RETRY  20
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG_STA = "wifi station";
+static const char *TAG_AP = "wifi softAP";
+
+static int s_retry_num = 0;
+/* FreeRTOS event group to signal when we are connected/disconnected */
+static EventGroupHandle_t s_wifi_event_group;
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *) event_data;
+        
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *) event_data;
+        
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+        ESP_LOGI(TAG_STA, "Station started");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+        ESP_LOGI(TAG_STA, "Got IP:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+esp_netif_t *wifi_init_softap(void)
+{
+    esp_netif_t *esp_netif_ap = esp_netif_create_default_wifi_ap();
+
+    wifi_config_t wifi_ap_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID_AP,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID_AP),
+            .channel = 2,
+            .password = EXAMPLE_ESP_WIFI_PASS_AP,
+            .max_connection = 4,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = {
+                .required = false,
+            },
+        },
+    };
+
+    if (strlen(EXAMPLE_ESP_WIFI_PASS_AP) == 0) {
+        wifi_ap_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_ap_config));
+
+    ESP_LOGI(TAG_AP, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             EXAMPLE_ESP_WIFI_SSID_AP, EXAMPLE_ESP_WIFI_PASS_AP, 2);
+
+    return esp_netif_ap;
+}
+
+/* Initialize wifi station */
+esp_netif_t *wifi_init_sta(void)
+{
+    esp_netif_t *esp_netif_sta = esp_netif_create_default_wifi_sta();
+
+    wifi_config_t wifi_sta_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID_STA,
+            .password = EXAMPLE_ESP_WIFI_PASS_STA,
+            .scan_method = WIFI_ALL_CHANNEL_SCAN,
+            .failure_retry_cnt = EXAMPLE_ESP_MAXIMUM_RETRY,
+            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (pasword len => 8).
+             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
+             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
+            * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
+             */
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_sta_config) );
+
+    ESP_LOGI(TAG_STA, "wifi_init_sta finished.");
+
+    return esp_netif_sta;
+}
+
+void softapsta(void){
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    /* Initialize event group */
+    s_wifi_event_group = xEventGroupCreate();
+
+    /* Register Event handler */
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                    ESP_EVENT_ANY_ID,
+                    &wifi_event_handler,
+                    NULL,
+                    NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                    IP_EVENT_STA_GOT_IP,
+                    &wifi_event_handler,
+                    NULL,
+                    NULL));
+
+    /*Initialize WiFi */
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+
+    /* Initialize AP */
+    ESP_LOGI(TAG_AP, "ESP_WIFI_MODE_AP");
+    esp_netif_t *esp_netif_ap = wifi_init_softap();
+
+    /* Initialize STA */
+    ESP_LOGI(TAG_STA, "ESP_WIFI_MODE_STA");
+    esp_netif_t *esp_netif_sta = wifi_init_sta();
+
+    /* Start WiFi */
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    
+
+    /* Set sta as the default interface */
+    esp_netif_set_default_netif(esp_netif_sta);
+
+    /* Enable napt on the AP netif */
+    /*if (esp_netif_napt_enable(esp_netif_ap) != ESP_OK) {
+        ESP_LOGE(TAG_STA, "NAPT not enabled on the netif: %p", esp_netif_ap);
+    }*/
+}
--- proyefinal2023/main/main.c
+++ proyefinal2023/main/main.c
@@ -8,7 +8,6 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-#include "protocol_examples_common.h"
 #include "esp_netif.h"
 #include "esp_netif_types.h"
 #include "lwip/ip_addr.h"
@@ -17,259 +16,10 @@
 #include "lwip/netdb.h"
 //#include "mdns.h"
 #include "mqtt_client.h"
+#include "softAPSTA.h"
+#include "MQTTThings.h"
 
 
-
-
-/* The examples use WiFi configuration that you can set via project configuration menu
-   If you'd rather not, just change the below entries to strings with the config you want
-   - ie #define EXAMPLE_ESP_WIFI_SSID "mywifissid" */
-
-#define EXAMPLE_ESP_WIFI_SSID_STA      "MaryLuWiFi-2.4GHz"
-#define EXAMPLE_ESP_WIFI_PASS_STA      "lu5291278"
-#define EXAMPLE_ESP_MAXIMUM_RETRY_STA  20
-
-#define EXAMPLE_ESP_WIFI_SSID_AP      "kaluga_wifi"
-#define EXAMPLE_ESP_WIFI_PASS_AP      "kalugaucu1"
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
-
-static const char *TAG_STA = "wifi station";
-static const char *TAG_AP = "wifi softAP";
-
-static int s_retry_num_sta = 0;
-
-static esp_netif_t *s_ap_netif ;
-
-
-
-
-
-
-static void event_handler_sta(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num_sta < EXAMPLE_ESP_MAXIMUM_RETRY_STA) {
-            esp_wifi_connect();
-            s_retry_num_sta++;
-            ESP_LOGI(TAG_STA, "RETRY TO CONNECT TO THE AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG_STA,"CONNECT TO THE AP FAIL");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG_STA, "GOT IP:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num_sta = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }
-}
-
-
-static void event_handler_ap(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        //ESP_LOGI(TAG_AP, "STATION " MACSTR " JOIN, AID=%d", MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        //ESP_LOGI(TAG_AP, "STATION " MACSTR " LEAVE, AID=%d", MAC2STR(event->mac), event->aid);
-    }
-}
-
-void wifi_init_sta(void)
-{
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler_sta, NULL, &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler_sta, NULL, &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID_STA,
-            .password = EXAMPLE_ESP_WIFI_PASS_STA,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG_STA, "WIFI_INIT_STA FINISHED.");
-
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
-
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG_STA, "CONNECTED TO AP SSID:%s PASSWORD:%s", EXAMPLE_ESP_WIFI_SSID_STA, EXAMPLE_ESP_WIFI_PASS_STA);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG_STA, "FAILED TO CONNECT TO AP SSID:%s, PASSWORD:%s", EXAMPLE_ESP_WIFI_SSID_STA, EXAMPLE_ESP_WIFI_PASS_STA);
-    } else {
-        ESP_LOGE(TAG_STA, "UNEXPECTED EVENT");
-    }
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
-    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
-    vEventGroupDelete(s_wifi_event_group);
-}
-
-void wifi_init_softap(void)
-{
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler_ap, NULL, &instance_any_id));
-
-    wifi_config_t wifi_config_ap = {
-        .ap = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID_AP,
-            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID_AP),
-            .channel = 2,
-            .password = EXAMPLE_ESP_WIFI_PASS_AP,
-            .max_connection = 4,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
-    };
-    if (strlen(EXAMPLE_ESP_WIFI_PASS_AP) == 0) {
-        wifi_config_ap.ap.authmode = WIFI_AUTH_OPEN;
-    }
-    
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config_ap));
-
-    ESP_LOGI(TAG_AP, "WIFI_INIT_SOFTAP FINISHED. SSID:%s PASSWORD:%s", EXAMPLE_ESP_WIFI_SSID_AP, EXAMPLE_ESP_WIFI_PASS_AP);
-
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
-}
-
-
-
-#define BROKER_URL "mqtt://demo.thingsboard.io"
-#define BROKER_PORT 1883
-#define CLIENT_ID "clientId-1I2csjHPrr"
-#define TOPIC "v1/devices/me/telemetry"
-#define BROKER_USER "79p8v7vt7232m1fzff2t"
-#define BROKER_PASS ""
-
-
-
-static const char *TAG = "MQTT_EXAMPLE";
-
-esp_mqtt_client_handle_t client;
-
-static void log_error_if_nonzero(const char *message, int error_code)
-{
-    if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
-    }
-}
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
-    esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-    switch ((esp_mqtt_event_id_t)event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-        }
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
-    }
-}
-
-
-
-void publish_telemetry(esp_mqtt_client_handle_t client, const char* topic, const char* data)
-{
-    esp_mqtt_client_publish(client, topic, data, strlen(data), 0, 0);
-}
-
-
-
-void mqtt_app_start(void)
-{
-    esp_err_t err = esp_wifi_stop();
-
-    
-    esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = BROKER_URL,
-        .broker.address.port = BROKER_PORT,
-        .credentials.username = BROKER_USER,
-        .credentials.authentication.password = BROKER_PASS,
-        
-
-        //.credentials.client_id = CLIENT_ID,
-        //.event_handler = mqtt_event_handler_cb,
-
-    };
-    client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-    esp_mqtt_client_start(client);
-    
-}
 
 
 
@@ -282,24 +32,15 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG_STA, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
+    softapsta(); // seteo y conexión en modo ap+sta hibrido
+    vTaskDelay(10000/portTICK_PERIOD_MS); //delay para esperar conexion a station, mejorar con err check
+    mqtt_app_start(); // inicia conexion a mqtt con dashboard de thingsboard predefinido
 
-    ESP_LOGI(TAG_AP, "ESP_WIFI_MODE_AP");
-    wifi_init_softap();
-
-    
-    
-   
-
-    
-    
-    mqtt_app_start();
     
 
     while (1){
 
-        publish_telemetry(client, TOPIC, "{\"temperature\": 24.5, \"humidity\": 70.2}");
+        publish_telemetry("{\"temperature\": 360.5, \"humidity\": 10.2}");
         vTaskDelay(5000/portTICK_PERIOD_MS);
     }
 }
